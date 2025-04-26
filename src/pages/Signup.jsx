78f6b0adc06a6d74d9993337731dd390ed1af531
LakshyaVerma123kl <lakshya123kl@gmail.com>
2025-04-26 17:08:05 +0530

    Fix Signup to use API service and remove hardcoded URL

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import api from "../services/api"; // Import the API service
 import {
   Container,
   Typography,
@@ -26,7 +26,7 @@ const Signup = () => {
     setLoading(true);
     setError("");
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/signup", {
+      const res = await api.post("/auth/signup", {
         email,
         password,
         name,
